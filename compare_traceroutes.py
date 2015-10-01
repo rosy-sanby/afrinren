5f39324957e7a9ecc43e2bd2e589c3a6e6ea0d4a
Roslyn Sanby <snbros001@trout.cs.uct.ac.za>
2015-10-01 11:55:55 +0200

    fixed up comparisons to figure out which destinations aren't working

--- compare_traceroutes.py
+++ compare_traceroutes.py
@@ -51,7 +51,7 @@ for filename in os.listdir(folder):
             probe_ids[dst_addr].append(prb_id)    
 
         latency[number][prb_id] = str(result["latency"])
-        reached_dest[number][prb_id] = True
+        reached_dest[number][prb_id] = False
         num_x_hops[number][prb_id] = 0
         for hop in result["result"]:
             hop_number = hop["hop"]
@@ -61,17 +61,20 @@ for filename in os.listdir(folder):
                 num_consec_hops[number][prb_id] = hop_number
 
             if hop["result"].has_key("x"):
-                if hop_number==255:
-                    reached_dest[number][result["prb_id"]] = False
+#                if hop_number==255:
+                    #reached_dest[number][result["prb_id"]] = False
                 num_x_hops[number][prb_id] += 1
+                
+            elif hop["result"]["from"]==dst_addr:
+                reached_dest[number][prb_id] = True
         num_hops[number][prb_id]=hop_number
         
-        
-#print(traceroutes)
-
-#best_result ={}
+       
 
 for dst_ip in traceroutes:
+    an_ip_reached = False
+    count=0
+    measurement_info = []
     results = {}
     gen_filename = folder+"/joined_with_paris/"+"hops_to_"+dst_ip+".json"
     final_result = []
@@ -80,16 +83,18 @@ for dst_ip in traceroutes:
         results[number] = json.load(json_file)
         json_file.close()
 
-   # print ("Destination: "+dst_ip)
     
     for prb_id in probe_ids[dst_ip]:
         best_result=0
-        #  print("Probe: "+str(prb_id))
         for number in traceroutes[dst_ip]:
             if reached_dest[number].has_key(prb_id):
                 if reached_dest[number][prb_id]:
-#                    print("Number: "+str(number)+", Protocol: "+protocols[number]+", Latency: "+latency[number][prb_id]+", # hops: "+str(num_hops[number][prb_id])+", # x hops: "+str(num_x_hops[number][prb_id]))
-
+                    an_ip_reached = True
+                    count+=1
+                    if count==1:
+                        measurement_info=[[str(prb_id),str(dst_ip),str(number)]]
+                    else:
+                        measurement_info.append([str(prb_id),str(dst_ip),str(number)])
                     if best_result<=0:
                         best_result = number
                     else:
@@ -102,7 +107,6 @@ for dst_ip in traceroutes:
                             best_result = number
                 else:
                     #Dest not reached for protocol
-                    #print("FAILED: Number: "+str(number)+", Protocol: "+protocols[number]+", Latency: "+latency[number][prb_id]+", # hops: "+str(num_hops[number][prb_id])+", # x hops: "+str(num_x_hops[number][prb_id]))
                     if best_result<=0:
                         best_result = number
                     else:
@@ -123,9 +127,16 @@ for dst_ip in traceroutes:
                     else:
                         final_result.append(result)
                     break
+    if not an_ip_reached:
+        print("no measurement from probe "+str(prb_id)+" to dest "+str(dst_ip))
+    
+    if len(measurement_info)<4:
+        for row in measurement_info:
+            for col in row:
+                print(col, end=" ")
+            print()
+        print()
 
-            #print("BEST: Number: "+str(number)+", Protocol: "+protocols[number]+", Latency: "+latency[number][prb_id]+", # hops: "+str(num_hops[number][prb_id])+", # x hops: "+str(num_x_hops[number][prb_id]))
-#    print()
     with open(gen_filename,'w') as gen_file:
         gen_file.write(json.dumps(final_result))
 
